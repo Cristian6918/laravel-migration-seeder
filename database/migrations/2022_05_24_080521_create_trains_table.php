b170defb037e5d962d62e0ccc2adaced38011667
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-05-24 10:33:47 +0200

    db created

---
+++ database/migrations/2022_05_24_080521_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('departure_statione');
+            $table->string('arrival_statiom');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->unsignedInteger('train_code')->unique();
+            $table->unsignedInteger('n_carriage');
+            $table->boolean('on_time');
+            $table->boolean('deleted');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
